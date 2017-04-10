c96c1e166197f61aa5dcb2af80f75d61c457f5d4
Lehoczky Péter <lehoczky.peter@fornax.hu>
2017-04-10 16:55:19 +0200

    syntax fix

--- html/pokemons.js
+++ html/pokemons.js
@@ -1,56 +1,56 @@
 var pokes = [
-{
-  "id": 1,
-  "name": "Bulbasaur",
-  "type": [ "grass", "posion" ],
-  "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
-},
-{
-  "id": 5,
-  "name": "Charmeleon",
-  "type": [ "fire" ],
-  "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png"
-},
-{
-  "id": 15,
-  "name": "Beedrill",
-  "type": [ "bug", "posion" ],
-  "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/15.png"
-},
-{
-  "id": 193,
-  "name": "Yanma",
-  "type": [ "bug", "flying" ],
-  "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/193.png"
-},
-{
-  "id": 152,
-  "name": "Chikorita",
-  "type": [ "grass" ],
-  "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/152.png"
-},
+    {
+        "id": 1,
+        "name": "Bulbasaur",
+        "type": ["grass", "posion"],
+        "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
+    },
+    {
+        "id": 5,
+        "name": "Charmeleon",
+        "type": ["fire"],
+        "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png"
+    },
+    {
+        "id": 15,
+        "name": "Beedrill",
+        "type": ["bug", "posion"],
+        "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/15.png"
+    },
+    {
+        "id": 193,
+        "name": "Yanma",
+        "type": ["bug", "flying"],
+        "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/193.png"
+    },
+    {
+        "id": 152,
+        "name": "Chikorita",
+        "type": ["grass"],
+        "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/152.png"
+    }
 ];
 
 var colors = {
-    "fire" : "#F08030",
-    "grass" : "#78C850",
-    "poison" : "#A040A0",
-    "water" : "#6890F0",
-    "bug" : "#A8B820",
-    "flying" : "#A890F0"
-}
+    "fire": "#F08030",
+    "grass": "#78C850",
+    "poison": "#A040A0",
+    "water": "#6890F0",
+    "bug": "#A8B820",
+    "flying": "#A890F0"
+};
 
 console.log(pokes);
 
 var ul = document.querySelector('ul');
-pokes.forEach(function(element){
+pokes.forEach(function (element) {
     var li = document.createElement('li');
     var img = document.createElement('img');
     var btn = document.createElement('button');
     var span = document.createElement('span');
     var p = document.createElement('p');
     li.className = "item-container";
-    element.type.forEach(function (type){
+    element.type.forEach(function (type) {
         li.className += ' ' + type;
     });
     img.src = element.sprite;
