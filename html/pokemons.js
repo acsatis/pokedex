3b370c811aacdf1e148cb7edd7f2eef594af4dce
Lehoczky Péter <lehoczky.peter@fornax.hu>
2017-04-12 13:53:24 +0200

    async loading with progress indicators

--- html/pokemons.js
+++ html/pokemons.js
@@ -1,148 +1,114 @@
-var pokes = [
-    {
-        "id": 1,
-        "name": "Bulbasaur",
-        "type": ["grass", "poison"],
-        "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
-    },
-    {
-        "id": 5,
-        "name": "Charmeleon",
-        "type": ["fire"],
-        "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png"
-    },
-    {
-        "id": 15,
-        "name": "Beedrill",
-        "type": ["bug", "poison"],
-        "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/15.png"
-    },
-    {
-        "id": 193,
-        "name": "Yanma",
-        "type": ["bug", "flying"],
-        "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/193.png"
-    },
-    {
-        "id": 152,
-        "name": "Chikorita",
-        "type": ["grass"],
-        "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/152.png"
-    }
-];
-
-var colors = {
-    "fire": "#F08030",
-    "grass": "#78C850",
-    "poison": "#A040A0",
-    "water": "#6890F0",
-    "bug": "#A8B820",
-    "flying": "#A890F0"
+const colors = {
+    "fairy": "#D685AD",
+    "steel": "#B7B7CE",
+    "dark": "#705746",
+    "dragon": "#6F35FC",
+    "ghost": "#735797",
+    "rock": "#B6A136",
+    "psychic": "#F95587",
+    "ground": "#E2BF65",
+    "fighting": "#C22E28",
+    "ice": "#96D9D6",
+    "electric": "#F7D02C",
+    "normal": "#A8A77A",
+    "fire": "#EE8130",
+    "grass": "#7AC74C",
+    "poison": "#A33EA1",
+    "water": "#6390F0",
+    "bug": "#A6B91A",
+    "flying": "#A98FF3"
 };
+const spinner = document.querySelector(".spinner");
+const filterPanel = document.querySelector("#filterPanel");
+const filter = document.querySelector("#filter");
+const ul = document.querySelector('ul');
+let pokemons;
 
-var itemcountSpan = document.querySelector("#itemcount");
-var downloadPanel = document.querySelector("#downloadingPanel");
-//var content = document.querySelector("#content");
-var filter = document.querySelector('input');
-var downloadedPokemons;
-//console.log(pokes);
-
-
-fetch("//pokeapi.co/api/v2/pokemon/?limit=5")
+fetch("//pokeapi.co/api/v2/pokemon/?limit=12")
     .then(function (response) {
         return response.text();
     })
     .then(function (body) {
         console.log(body);
-        var parsedBody = JSON.parse(body);
+        const parsedBody = JSON.parse(body);
         console.log(parsedBody.results);
-        var pokemons = parsedBody.results.map(function (poke) {
-            return {
-                "id": 1,
-                "name": poke.name,
-                "type": [],
-                "sprite": ""
-            }
-        });
-
-        downloadPokeDetails(parsedBody.results, pokemons, pokemons.length);
+        pokemons = parsedBody.results.map(pokemon => ({
+            "id": parseInt(pokemon.url.match(/(\d+)(?!.*\d)/)[0], 10),
+            "url": pokemon.url,
+            "name": pokemon.name,
+            "type": [],
+            "sprite": "",
+            "searchString": pokemon.name
+        }));
+        spinner.style.display = "none";
+        filterPanel.style.display = "block";
+        renderPokemons(pokemons);
+        downloadPokeDetails(pokemons);
     })
     .catch(function (ex) {
         console.log("parsing failed", ex);
     });
 
-function downloadPokeDetails(serverpokemons, pokemons, count) {
-    console.log("item count", count);
-    // console.log("url:", serverpokemons[count-1].url);
-    fetch(serverpokemons[count - 1].url)
-        .then(function (response) {
-            return response.text();
-        })
-        .then(function (body) {
-            var parsedBody = JSON.parse(body);
-            pokemons[count - 1].type = parsedBody.types.map(function (details) {
-                return details.type.name;
+function downloadPokeDetails() {
+    pokemons.forEach(pokemon => {
+        fetch(pokemon.url)
+            .then(response => response.text())
+            .then(body =>{
+                const downloadedPokemon = JSON.parse(body);
+                pokemon.type = downloadedPokemon.types.map(details =>{
+                    const type = details.type.name;
+                    pokemon.searchString += "###" + type;
+                    return type;
+                });
+                pokemon.sprite = downloadedPokemon.sprites.front_default;
+                renderPokemons(pokemons);
+            })
+            .catch(ex =>{
+                console.log("Error details", ex);
             });
-            pokemons[count - 1].sprite = parsedBody.sprites.front_default;
-            if (count === 1) {
-                downloadedPokemons = pokemons;
-                renderPokemons(pokemons);
-                downloadPanel.parentNode.removeChild(downloadPanel);
-            }
-            else {
-                downloadPokeDetails(serverpokemons, pokemons, count - 1);
-            }
-            itemcountSpan.textContent = count - 1;
-        })
-        .catch(function (ex) {
-            console.log("Error details", ex);
-            if (count === 1) {
-                downloadedPokemons = pokemons;
-                renderPokemons(pokemons);
-            }
-            else {
-                downloadPokeDetails(serverpokemons, pokemons, count - 1);
-            }
-        });
+    })
 }
 
-
-function renderPokemons(pokemons) {
-    filter.style.display = "block";
-    var ul = document.querySelector('ul');
+function renderPokemons(pokemonsToRender) {
     ul.innerHTML = "";
-    pokemons.forEach(function (element) {
-        var li = document.createElement('li');
-        var img = document.createElement('img');
-        var btn = document.createElement('button');
-        var span = document.createElement('span');
-        var p = document.createElement('p');
-        li.className = "item-container";
-        if (element.type.length === 1) {
-            li.style.background = colors[element.type[0]];
-        } else {
-            var bg = "background: linear-gradient(90deg, ";
-            element.type.forEach(function (type) {
-                bg += colors[type] + " 50%, ";
-            });
-            bg = bg.substring(0, bg.length - 2);
-            bg += ");";
-            li.setAttribute('style', bg);
-        }
-        img.src = element.sprite;
-        span.className = "item-name";
-        span.textContent = element.name;
-        span.appendChild(p);
-        btn.appendChild(img);
-        li.appendChild(btn);
-        li.appendChild(span);
-        ul.appendChild(li);
+    pokemonsToRender.forEach(pokemon =>{
+        ul.appendChild(renderPokemon(pokemon));
     });
 }
 
-filter.addEventListener("input", function (event) {
-    renderPokemons(downloadedPokemons.filter(function (pokemon) {
-        return pokemon.name.startsWith(event.target.value);
-    }));
+function renderPokemon(pokemon) {
+    const li = document.createElement('li');
+    const btn = document.createElement('button');
+    const img = document.createElement('img');
+    const span = document.createElement('span');
+    const p = document.createElement('p');
+    li.className = "item-container";
+    if (pokemon.type.length === 1) {
+        li.style.background = colors[pokemon.type[0]];
+    } else {
+        let bg = "background: linear-gradient(90deg, ";
+        pokemon.type.forEach(type =>{
+            bg += colors[type] + " 50%, ";
+        });
+        bg = bg.substring(0, bg.length - 2);
+        bg += ");";
+        li.setAttribute('style', bg);
+    }
+    span.className = "item-name";
+    span.textContent = pokemon.name;
+    span.appendChild(p);
+    btn.appendChild(img);
+    li.appendChild(btn);
+    if ("" === pokemon.sprite) {
+        img.src = "spinner.gif"
+    } else {
+        img.src = pokemon.sprite;
+    }
+    li.appendChild(span);
+    return li;
+}
+
+filter.addEventListener("input", event =>{
+    renderPokemons(pokemons.filter(pokemon => pokemon.searchString.indexOf(event.target.value) !== -1));
 });
 
